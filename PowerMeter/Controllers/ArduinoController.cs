f21bccc0864a03de230966a702a3c61d4a6abf51
Marcin Gluza <gumball300@gmail.com>
2019-02-25 00:23:26 +0100

    POST

---
+++ PowerMeter/Controllers/ArduinoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PowerMeter.Controllers
+{
+    public class ArduinoController : Controller
+    {
+        // 
+        // GET: /Arduino
+
+        public string Index()
+        {
+            return "This is my <b>Arduino</b> action...";
+        }
+
+        // 
+        // GET: /Arduino/Add 
+        public string Add(string id, int voltage, float l1_current, float l2_current, float l3_current)
+
+        {
+            return HttpUtility.HtmlEncode("ID " + id + ", Voltage: " + voltage);
+        }
+
+    }
+}
